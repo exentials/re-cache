db2df088e3b90b1d7084ff0b48b80d68953713c4
Damiano <damiano.g@outlook.it>
2022-12-14 19:48:53 +0100

    Added cli builder and version display

--- src/Exentials.ReCache.ReCli/Program.cs
+++ src/Exentials.ReCache.ReCli/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Exentials.ReCache.ReCli.Commands;
 using System.CommandLine;
+using System.Reflection;
 
 class Program
 {
     static async Task<int> Main(string[] _)
     {
-        var recli = new ReCacheRootCommand();
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
 
-        Console.WriteLine("Welcome to ReCache Cli");
+        Console.WriteLine($"Welcome to ReCache Cli {version}");
         Console.WriteLine("Digit -h for command help.");
         Console.Write("> ");
+
         string? cmd;
+        var recli = new ReCacheRootCommand();
         while ((cmd = Console.ReadLine()) != "exit")
         {
             if (!string.IsNullOrWhiteSpace(cmd))
